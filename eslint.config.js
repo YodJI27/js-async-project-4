4026296b1d941cec7612fb05111348e59b4cde27
YodJI27 <yodji2308@mail.ru>
2025-05-26 19:09:45 +0300

    step 6

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,13 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default [
   js.configs.recommended,
   {
+     plugins: {
+      '@stylistic': stylistic,
+    },
     languageOptions: {
       globals: {
         ...globals.node,
